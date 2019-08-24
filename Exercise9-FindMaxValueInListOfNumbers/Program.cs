97f0f3110d85b14878879b624494168e67e9b9a6
Frank Jamison <frankjamison@gmail.com>
2019-08-24 01:46:22 -0700

    Exercise 9: Find max value in list of comma separated numbers
    Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.

---
+++ Exercise9-FindMaxValueInListOfNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise9_FindMaxValueInListOfNumbers
+{
+    /*
+     * Write a program and ask the user to enter a series of numbers separated by comma. 
+     * Find the maximum of the numbers and display it on the console. 
+     * For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get input from user
+            Console.WriteLine("Please enter a list of comma separated numbers: ");
+            var input = Console.ReadLine();
+
+            // Separate numbers into list
+            var numbers = input.Split(',');
+
+            // Max Value
+            var maxValue = Convert.ToInt32(numbers[0]);
+
+            // Loop through numbers to determine max value
+            foreach (var value in numbers)
+            {
+                // Convert value to integer
+                var thisNumber = Convert.ToInt32(value);
+
+                // Determine max value
+                if (thisNumber > maxValue)
+                {
+                    maxValue = thisNumber;
+                }
+            }
+
+            // Display max value
+            Console.WriteLine("The highest number is {0}", maxValue);
+        }
+    }
+}
