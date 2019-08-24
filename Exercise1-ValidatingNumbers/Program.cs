e704265408261c3a74f0a3ddb8ab5f91b4c4c655
Frank Jamison <frankjamison@gmail.com>
2019-08-24 01:09:43 -0700

    Exercise 1: Validating Numbers
    Write a program and ask the user to enter a number. The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)

---
+++ Exercise1-ValidatingNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise1_ValidatingNumbers
+{
+    /* 
+    * Write a program and ask the user to enter a number. 
+    * The number should be between 1 to 10. 
+    * If the user enters a valid number, display "Valid" on the console. 
+    * Otherwise, display "Invalid".
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get number from user
+            Console.WriteLine("Please enter a number between 1 and 10.");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            // Display validity of number entered
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
