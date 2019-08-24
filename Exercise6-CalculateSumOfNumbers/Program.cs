4600d0e6f3db7fa152d9974e43cf78c79d4b7627
Frank Jamison <frankjamison@gmail.com>
2019-08-24 01:34:37 -0700

    Exercise 6: Calculate sum of numbers entered
    Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.

---
+++ Exercise6-CalculateSumOfNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercise6_CalculateSumOfNumbers
+{
+    /*
+     * Write a program and continuously ask the user to enter a number or "ok" to exit. 
+     * Calculate the sum of all the previously entered numbers and display it on the console.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+            var input = "";
+
+            while (true)
+            {
+                // Get user input
+                Console.WriteLine("Please enter a number or enter 'OK' to exit: ");
+                input = Console.ReadLine();
+
+                // Break out of loop if input is "OK"
+                if (input == "OK")
+                {
+                    break;
+                }
+
+                // Add input number to sum
+                sum += Convert.ToInt32(input);
+            }
+
+            // Display sum of numbers
+            Console.WriteLine("The sum of the numbers entered is " + sum);
+        }
+    }
+}
