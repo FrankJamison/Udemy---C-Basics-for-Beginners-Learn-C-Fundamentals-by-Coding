d47449c095ec88e3a65ef619e62f284f0e59bcc2
Frank Jamison <frankjamison@gmail.com>
2019-08-27 16:44:53 -0700

    Exercise 11: Name Reversal
    Write a program and ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. Display the reversed name on the console.

---
+++ Exercise11-NameReversal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise11_NameReversal
+{
+    /*
+     * Write a program and ask the user to enter their name. 
+     * Use an array to reverse the name and then store the result in a new string. 
+     * Display the reversed name on the console.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a name from the user
+            Console.WriteLine("Please enter a name: ");
+            var name = Console.ReadLine();
+
+            // Create an array for the name
+            var nameArray = new char[name.Length];
+
+            // Populate name array with reversed string characters
+            for (int i = name.Length; i > 0; i--)
+            {
+                nameArray[name.Length - i] = name[i - 1];
+            }
+
+            // Convert Array to String
+            var nameReversed = string.Join("", nameArray);
+
+            // Display the reversed name
+            Console.WriteLine("The name reversed is " + nameReversed);
+        }
+    }
+}
