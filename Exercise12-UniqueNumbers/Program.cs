4a3d7bd194a6fc9c8360a639b01f697eab5ea6c3
Frank Jamison <frankjamison@gmail.com>
2019-08-27 17:33:56 -0700

    Exercise 12: Unique Numbers
    Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.

---
+++ Exercise12-UniqueNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise12_UniqueNumbers
+{
+    /*
+     * Write a program and ask the user to enter 5 numbers. 
+     * If a number has been previously entered, display an error message and ask the user to re-try. 
+     * Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a list to hold the numbers
+            var numbers = new List<int>();
+
+            // Get 5 unique numbers from the user
+            while (numbers.Count < 5)
+            {
+                // Get a number from the user
+                Console.WriteLine("Plaese enter a number: ");
+                var number = Convert.ToInt32(Console.ReadLine());
+
+                // If the entered number is not unique, display error and prompt for new number.
+                while (numbers.IndexOf(number) >= 0)
+                {
+                    Console.WriteLine(number + "has already been entered. Please enter a unique number: ");
+                    number = Convert.ToInt32(Console.ReadLine());
+                }
+
+                // Add number to list of numbers
+                numbers.Add(number);
+            }
+
+            // Sort the list of numbers
+            numbers.Sort();
+
+            // Display the sorted list
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
