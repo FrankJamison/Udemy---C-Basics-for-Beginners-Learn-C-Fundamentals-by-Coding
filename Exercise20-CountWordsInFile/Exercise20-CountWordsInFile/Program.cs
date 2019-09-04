06a84421750c9eb6c51dbd439046679d4d9bd23b
Frank Jamison <frankjamison@gmail.com>
2019-09-04 16:40:51 -0700

    Exercise 20: Count Words in File
    Write a program that reads a text file and displays the number of words.

---
+++ Exercise20-CountWordsInFile/Exercise20-CountWordsInFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise20_CountWordsInFile
+{
+    /*
+     * Write a program that reads a text file and displays the number of words.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Relative File Path
+            var file = @"../../../text.txt";
+            
+            // Display the number of words in a text file
+            Console.WriteLine(getNumberOfWords(file));
+        }
+
+        public static int getNumberOfWords(string file)
+        {
+            // Get text from file
+            var text = File.ReadAllText(file);
+
+            // Split text into words
+            var words = text.Split(' ');
+
+            // Return number of words
+            return words.Length;
+        }
+    }
+}
