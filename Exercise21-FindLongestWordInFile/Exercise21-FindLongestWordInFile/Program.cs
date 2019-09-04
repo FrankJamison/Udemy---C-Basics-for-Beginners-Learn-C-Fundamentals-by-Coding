4d2ca5eda0707d4dda38c1f154d458c802afa15a
Frank Jamison <frankjamison@gmail.com>
2019-09-04 16:53:45 -0700

    Exercise 21: Find the longest word in a file
    Write a program that reads a text file and displays the longest word in the file.

---
+++ Exercise21-FindLongestWordInFile/Exercise21-FindLongestWordInFile/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Exercise21_FindLongestWordInFile
+{
+    /*
+     * Write a program that reads a text file and displays the longest word in the file.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Relative File Path
+            var file = @"../../../text.txt";
+
+            // Get longest word from file
+            Console.WriteLine(getLongestWord(file));
+        }
+
+        public static string getLongestWord(string file)
+        {
+            // variables
+            var longestWord = "";
+            var highestLetterCount = 0;
+
+            //  Get text from file
+            var text = File.ReadAllText(file);
+
+            // Split text string into words
+            var words = text.Split(' ');
+
+            // Find longest word
+            foreach (var word in words)
+            {
+                if (word.Length > highestLetterCount)
+                {
+                    highestLetterCount = word.Length;
+                    longestWord = word;
+                }
+            }
+
+            return longestWord;
+        }
+    }
+}
