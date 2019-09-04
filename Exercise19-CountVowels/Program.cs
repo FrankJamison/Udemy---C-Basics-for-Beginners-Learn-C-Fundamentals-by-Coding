51dc7616aea0b68a6054d691d00c813530faab38
Frank Jamison <frankjamison@gmail.com>
2019-09-03 20:09:59 -0700

    Exercise 19: Vowel Count
    Write a program and ask the user to enter an English word. Count the number of vowels (a, e, o, u, i) in the word. So, if the user enters "inadequate", the program should display 6 on the console.

---
+++ Exercise19-CountVowels/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise19_CountVowels
+{
+    /*
+     * Write a program and ask the user to enter an English word. 
+     * Count the number of vowels (a, e, o, u, i) in the word. 
+     * So, if the user enters "inadequate", the program should display 6 on the console.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get input from the user
+            Console.WriteLine("Please enter a word: ");
+            var input = Console.ReadLine();
+
+            // Check for null or whitespace
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            // Vowel Count
+            var vowelCount = 0;
+
+            foreach (var letter in input.ToLower())
+            {
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                {
+                    vowelCount++;
+                }
+            }
+
+            // Display vowel count
+            Console.WriteLine(vowelCount);
+        }
+    }
+}
